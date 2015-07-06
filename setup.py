a8ffeb3b7d684ed43b9c4e76f13a6049a9606f2d
Daniel Manson <d.manson.11@ucl.ac.uk>
2015-07-06 12:18:51 +0100

    rebrand in setup.py

--- setup.py
+++ setup.py
@@ -1,42 +1,28 @@
 #!/usr/bin/env python
 
-"""
-setup.py file 
-"""
 
-#%% Load packages
-from setuptools import setup, find_packages  # Always prefer setuptools over distutils
+from setuptools import setup
 
-#from codecs import open  # To use a consistent encoding
-from os import path
-import os,sys
-import numpy as np
-import platform
-
-
-#%% Test suite
-
-
-packages=['aggregate']
     
-setup (name = 'aggregate',
+setup (name = 'numpy-groupies',
        version = '0.9',
        author      = "@ml31415 and @d1manson",
-       description = "Replacement for Matlab's accumarray function.",
+       description = "Optimised tools for group-indexing operations: aggregated sum and more.",
+       url="https://github.com/ml31415/numpy-groupies",
+       download_url="https://github.com/ml31415/numpy-groupies/archive/master.zip",
        author_email='',
-	license="Not set",
-	keywords=[ "accumarray aggregate"],
-        packages=packages,
-	tests_require=[''],
-        zip_safe=False,
-	requires=['numpy'],
-	classifiers=['Development Status :: 4 - Beta', 'Intended Audience :: Science/Research', 
-	      'Programming Language :: Python :: 2',
-	      'Programming Language :: Python :: 2.7',
-	      'Programming Language :: Python :: 3',
-	      'Programming Language :: Python :: 3.3',
-	      'Programming Language :: Python :: 3.4', 
-	      ]
+       license="Not set",
+       keywords=[ "accumarray", "aggregate", "groupby", "grouping", "indexing"],
+       packages=['numpy-groupies'],
+       tests_require=[''],
+       requires=[],
+       classifiers=['Development Status :: 4 - Beta', 'Intended Audience :: Science/Research', 
+          	         'Programming Language :: Python :: 2',
+           	         'Programming Language :: Python :: 2.7',
+        	         'Programming Language :: Python :: 3',
+        	         'Programming Language :: Python :: 3.3',
+        	         'Programming Language :: Python :: 3.4', 
+          	        ]
        )
 
 
