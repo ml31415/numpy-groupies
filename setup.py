afa5f3a016f5b8158fc4e43f60f78ef40bd47b11
Michael Löffler <ml@redcowmedia.de>
2016-04-12 14:55:20 +0300

    License set to public domain

--- setup.py
+++ setup.py
@@ -1,25 +1,23 @@
 #!/usr/bin/env python
 
-
 from setuptools import setup
 
-setup (name = 'numpy_groupies',
-       version = '0.9.1',
-       author      = "@ml31415 and @d1manson",
-       description = "Optimised tools for group-indexing operations: aggregated sum and more.",
-       url="https://github.com/ml31415/numpy-groupies",
-       download_url="https://github.com/ml31415/numpy-groupies/archive/master.zip",
-       keywords=[ "accumarray", "aggregate", "groupby", "grouping", "indexing"],
-       packages=['numpy_groupies'],
-       tests_require=[''],
-       requires=[],
-       classifiers=['Development Status :: 4 - Beta', 'Intended Audience :: Science/Research', 
-          	         'Programming Language :: Python :: 2',
-           	         'Programming Language :: Python :: 2.7',
-        	         'Programming Language :: Python :: 3',
-        	         'Programming Language :: Python :: 3.3',
-        	         'Programming Language :: Python :: 3.4', 
-          	        ]
-       )
-
-
+setup(name='numpy_groupies',
+      version='0.9.1',
+      author="@ml31415 and @d1manson",
+      license='Public Domain',
+      description="Optimised tools for group-indexing operations: aggregated sum and more.",
+      url="https://github.com/ml31415/numpy-groupies",
+      download_url="https://github.com/ml31415/numpy-groupies/archive/master.zip",
+      keywords=[ "accumarray", "aggregate", "groupby", "grouping", "indexing"],
+      packages=['numpy_groupies'],
+      tests_require=[''],
+      requires=[],
+      classifiers=['Development Status :: 4 - Beta', 'Intended Audience :: Science/Research',
+                   'Programming Language :: Python :: 2',
+                   'Programming Language :: Python :: 2.7',
+                   'Programming Language :: Python :: 3',
+                   'Programming Language :: Python :: 3.3',
+                   'Programming Language :: Python :: 3.4',
+                   ]
+      )
