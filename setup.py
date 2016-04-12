f233c287fe5e4f963d66004fc60b8725095399a8
Michael Löffler <ml@redcowmedia.de>
2016-04-12 15:00:45 +0300

    Clean command added

--- setup.py
+++ setup.py
@@ -1,6 +1,38 @@
 #!/usr/bin/env python
 
+import os
 from setuptools import setup
+from distutils import log
+from distutils.command.clean import clean
+from distutils.dir_util import remove_tree
+
+base_path = os.path.dirname(os.path.abspath(__file__))
+
+class NumpyGroupiesClean(clean):
+    """Custom clean command to tidy up the project root."""
+    def run(self):
+        clean.run(self)
+        for folder in ('build', 'numpy_groupies.egg-info'):
+            path = os.path.join(base_path, folder)
+            if os.path.isdir(path):
+                remove_tree(path, dry_run=self.dry_run)
+        if not self.dry_run:
+            self._rm_walk()
+
+    def _rm_walk(self):
+        for path, dirs, files in os.walk(base_path):
+            if any(p.startswith('.') for p in path.split(os.path.sep)):
+                # Skip hidden directories like the git folder right away
+                continue
+            if path.endswith('__pycache__'):
+                remove_tree(path, dry_run=self.dry_run)
+            else:
+                for fname in files:
+                    if fname.endswith('.pyc') or fname.endswith('.so'):
+                        fpath = os.path.join(path, fname)
+                        os.remove(fpath)
+                        log.info("removing '%s'", fpath)
+
 
 setup(name='numpy_groupies',
       version='0.9.1',
@@ -19,5 +51,6 @@ setup(name='numpy_groupies',
                    'Programming Language :: Python :: 3',
                    'Programming Language :: Python :: 3.3',
                    'Programming Language :: Python :: 3.4',
-                   ]
+                   ],
+      cmdclass=dict(clean=NumpyGroupiesClean)
       )
